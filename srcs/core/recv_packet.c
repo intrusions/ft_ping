2b1921a267f97677d499f51835f5eb44b7f9dc17
intrusions <xeloronkali@gmail.com>
2024-07-05 19:29:23 +0200

    refacto(recv_packet): main function splitted

--- srcs/core/recv_packet.c
+++ srcs/core/recv_packet.c
@@ -26,32 +26,10 @@ static i8 verify_packet_integrity(t_data *data, struct iphdr *ip_hdr, struct icm
     return ERR_ICMP_DEFAULT;
 }
 
-void recv_packet(t_data *data, struct sockaddr_in *r_addr, socklen_t *addr_len, char *ip, u16 n_sequence, u16 *n_packet_received, struct timeval *start_time, struct timeval *end_time)
+void print_recv_err(i8 status_code, u8 packet_size, char *ip, u8 icmphdr_code)
 {
-    char response[PING_MAX_PKT_SIZE];
-    u8 ttl;
-    i32 bytes_received = 0;
-    memset(response, 0, sizeof(response));
-    
-    if ((bytes_received = recvfrom(data->sockfd, response, sizeof(response), 0, (struct sockaddr *)r_addr, addr_len)) <= 0) {
-        fprintf(stderr, "packet receive failed\n");
-        close(data->sockfd);
-        exit(EXIT_FAILURE);
-    }
-    
-    struct iphdr *ip_hdr = (struct iphdr *)response;
-    struct icmphdr *icmp_hdr = (struct icmphdr *)(response + (ip_hdr->ihl * 4));
-    u8 packet_size = bytes_received - sizeof(struct iphdr);
-    // print_received_packet(ip_hdr, icmp_hdr, response + 28);
+    switch (status_code) {
 
-    switch (verify_packet_integrity(data, ip_hdr, icmp_hdr, n_sequence, &ttl)) {
-        case ICMP_PACKET_SUCCESS:
-            ++*n_packet_received;
-            gettimeofday(end_time, NULL);
-            double time_elapsed = calcul_latency(*start_time, *end_time);
-            fprintf(stdout, "%d bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n", packet_size, ip, n_sequence, ttl, time_elapsed);
-            break;
-        
         case ERR_ICMP_TIME_EXCEEDED:
             fprintf(stdout, "%d bytes from %s: Time to Live exceeded (TTL expired)\n", packet_size, ip);
             break;
@@ -73,7 +51,36 @@ void recv_packet(t_data *data, struct sockaddr_in *r_addr, socklen_t *addr_len, 
             break;
         
         default:
-            fprintf(stdout, "%d bytes from %s: Destination Unreachable, code=%d\n", packet_size, ip, icmp_hdr->code);
+            fprintf(stdout, "%d bytes from %s: Destination Unreachable, code=%d\n", packet_size, ip, icmphdr_code);
             break;
+    }
+}
+
+void recv_packet(t_data *data, struct sockaddr_in *r_addr, socklen_t *addr_len, char *ip, u16 n_sequence, u16 *n_packet_received, struct timeval *start_time, struct timeval *end_time)
+{
+    char response[PING_MAX_PKT_SIZE];
+    u8 ttl;
+    i32 bytes_received = 0;
+    memset(response, 0, sizeof(response));
+    
+    if ((bytes_received = recvfrom(data->sockfd, response, sizeof(response), 0, (struct sockaddr *)r_addr, addr_len)) <= 0) {
+        fprintf(stderr, "packet receive failed\n");
+        close(data->sockfd);
+        exit(EXIT_FAILURE);
+    }
+    
+    struct iphdr *ip_hdr = (struct iphdr *)response;
+    struct icmphdr *icmp_hdr = (struct icmphdr *)(response + (ip_hdr->ihl * 4));
+    u8 packet_size = bytes_received - sizeof(struct iphdr);
+    // print_received_packet(ip_hdr, icmp_hdr, response + 28);
+
+    i8 status_code = verify_packet_integrity(data, ip_hdr, icmp_hdr, n_sequence, &ttl);
+    if (status_code == ICMP_PACKET_SUCCESS) {
+        ++*n_packet_received;
+        gettimeofday(end_time, NULL);
+        double time_elapsed = calcul_latency(*start_time, *end_time);
+        fprintf(stdout, "%d bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n", packet_size, ip, n_sequence, ttl, time_elapsed);
+    } else {
+        print_recv_err(status_code, packet_size, ip, icmp_hdr->code);
     }
 }
